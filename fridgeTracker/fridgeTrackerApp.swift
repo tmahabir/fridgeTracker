5c586140dbd8bb510973dfced6fe1ba1eb2ba556
Tenzin Mahabir <tmahabir10@gmail.com>
2021-08-04 20:37:46 -0400

    Initial Commit

---
+++ fridgeTracker/fridgeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  fridgeTrackerApp.swift
+//  fridgeTracker
+//
+//  Created by Tenzin Mahabir on 2021-08-04.
+//
+
+import SwiftUI
+
+@main
+struct fridgeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fridgeTrackerTests/fridgeTrackerTests.swift
@@ -0,0 +1,33 @@
+//
+//  fridgeTrackerTests.swift
+//  fridgeTrackerTests
+//
+//  Created by Tenzin Mahabir on 2021-08-04.
+//
+
+import XCTest
+@testable import fridgeTracker
+
+class fridgeTrackerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
