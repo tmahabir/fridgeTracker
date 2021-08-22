532333f377a4df6fe530f5b32ef7b540a6d5db39
Tenzin Mahabir <tmahabir10@gmail.com>
2021-08-22 00:14:27 -0600

    Improved the adding food view

--- fridgeTracker/Views/Helpers/FoodInput.swift
+++ fridgeTracker/Views/Helpers/FoodInput.swift
@@ -14,47 +14,91 @@ struct FoodInput: View {
     @State private var foodName: String = ""
     @State private var expiryDate: String = ""
     @State private var storage: String = ""
+    @State private var isFavorite = false
+    @State private var notes: String = ""
     @State private var isEditing = false
+    @State private var hasBeenAdded = false
+    
+    private var possibleAdd: some View {
+        if ( !hasBeenAdded && (foodName != "") && (expiryDate != "") && (storage != "")) {
+            
+            return AnyView(Button(action: { onAdd() }) {
+                Image(systemName: "plus")
+                })
+        }
+        
+        return AnyView(EmptyView())
+    }
     
     private func onAdd() {
-        modelData.landmarks.append(modelData.landmarks[0])
-        modelData.landmarks.append(Landmark(id:12, name: foodName, park: expiryDate, state: storage, description: "words", isFavorite: true))
+        modelData.landmarks.append(Landmark(id:12, name: foodName, park: expiryDate, state: storage, description: notes, isFavorite: isFavorite))
+        hasBeenAdded = true
     }
     
     var body: some View {
         NavigationView {
             List {
-            TextField(
-                    "Food name?",
-                     text: $foodName
-                ) { isEditing in
-                    self.isEditing = isEditing
-                }
-                .autocapitalization(.none)
-                .disableAutocorrection(true)
-                .border(Color(UIColor.separator))
-            TextField(
-                    "When does it expire?",
-                     text: $expiryDate
-                ) { isEditing in
-                    self.isEditing = isEditing
-                }
-                .autocapitalization(.none)
-                .disableAutocorrection(true)
-                .border(Color(UIColor.separator))
-            TextField(
-                    "Where are you storing it?",
-                     text: $storage
-                ) { isEditing in
-                    self.isEditing = isEditing
-                }
-                .autocapitalization(.none)
-                .disableAutocorrection(true)
-                .border(Color(UIColor.separator))
-            }.navigationTitle("Add a Food").navigationBarItems( trailing: Button(action: { onAdd() }) {
-                Image(systemName: "plus")
-                })
+                VStack(alignment: .leading) {
+                    
+                    HStack {
+                        TextField(
+                                "Food name?",
+                                 text: $foodName
+                            ) { isEditing in
+                                self.isEditing = isEditing
+                            }
+                            .autocapitalization(.none)
+                            .disableAutocorrection(true)
+                            .border(Color(UIColor.separator))
+                        .font(.title)
+                        .foregroundColor(.primary)
+                            
+                            FavoriteButton(isSet: $isFavorite)
+                    }
+                    
+                    Spacer()
+                
+                    HStack {
+                        TextField(
+                                "When does it expire?",
+                                 text: $expiryDate
+                            ) { isEditing in
+                                self.isEditing = isEditing
+                            }
+                            .autocapitalization(.none)
+                            .disableAutocorrection(true)
+                            .border(Color(UIColor.separator))
+                        TextField(
+                                "Where are you storing it?",
+                                 text: $storage
+                            ) { isEditing in
+                                self.isEditing = isEditing
+                            }
+                            .autocapitalization(.none)
+                            .disableAutocorrection(true)
+                            .border(Color(UIColor.separator))
+                        
+                    }.font(.subheadline)
+                    .foregroundColor(.secondary)
+                    
+                    Divider()
+
+                        Text("Notes on \(foodName)")
+                                            .font(.title2)
+                        TextField(
+                            "Notes",
+                             text: $notes
+                        ) { isEditing in
+                            self.isEditing = isEditing
+                        }
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                        .border(Color(UIColor.separator))
+                }.padding(.vertical)
+            
+            }.navigationTitle("Add a Food").navigationBarItems( trailing: possibleAdd)
         }
+        
     }
 }
 
